cb8cafd533a9355aabfe1203fe62cf82704d1692
geekinryxnn <ryankiratu67@gmail.com>
2025-04-11 10:21:52 +0300

    new changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import{image} from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about me.</p>
+      <img src={"https://i.imgur.com/mV8PQxj.gif"} alt="i made this" />
+    </div>
+  );
 }
 
 export default About;
